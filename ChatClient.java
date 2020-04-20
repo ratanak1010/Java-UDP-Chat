bc1e8b9912c6a15236c6b06c762da6e171335d61
ddugi <easthima@gmail.com>
2020-04-20 21:25:05 +0900

    2인용 채팅프로그램을 여러 사람이 사용할 수 있게 끔 코드 변경

--- ChatClient.java
+++ ChatClient.java
@@ -2,79 +2,41 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+class Chatclient extends Thread {
+	static ArrayList<PrintWriter> list = new ArrayList<PrintWriter>();
+	Socket socket;
+	PrintWriter writer;
 
-class MessageSender implements Runnable {
-    public final static int PORT = 7331;
-    private DatagramSocket sock;
-    private String hostname;
-    MessageSender(DatagramSocket s, String h) {
-        sock = s;
-        hostname = h;
-    }
-    private void sendMessage(String s) throws Exception {
-        byte buf[] = s.getBytes();
-        InetAddress address = InetAddress.getByName(hostname);
-        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, PORT);
-        sock.send(packet);
-    }
-    public void run() {
-        boolean connected = false;
-        do {
-            try {
-                sendMessage("GREETINGS");
-                connected = true;
-            } catch (Exception e) {
-                
-            }
-        } while (!connected);
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            try {
-                while (!in.ready()) {
-                    Thread.sleep(100);
-                }
-                sendMessage(in.readLine());
-            } catch(Exception e) {
-                System.err.println(e);
-            }
-        }
-    }
-}
-class MessageReceiver implements Runnable {
-    DatagramSocket sock;
-    byte buf[];
-    MessageReceiver(DatagramSocket s) {
-        sock = s;
-        buf = new byte[1024];
-    }
-    public void run() {
-        while (true) {
-            try {
-                DatagramPacket packet = new DatagramPacket(buf, buf.length);
-                sock.receive(packet);
-                String received = new String(packet.getData(), 0, packet.getLength());
-                System.out.println(received);
-            } catch(Exception e) {
-                System.err.println(e);
-            }
-        }
-    }
-}
-public class ChatClient {
-    
-    public static void main(String args[]) throws Exception {
-        String host = null;
-        if (args.length < 1) {
-            System.out.println("Usage: java ChatClient <server_hostname>");
-            System.exit(0);
-        } else {
-            host = args[0];
-        }
-        DatagramSocket socket = new DatagramSocket();
-        MessageReceiver r = new MessageReceiver(socket);
-        MessageSender s = new MessageSender(socket, host);
-        Thread rt = new Thread(r);
-        Thread st = new Thread(s);
-        rt.start(); st.start();
-    }
+	Chatclient(Socket socket) {
+		this.socket = socket;
+		try {
+			writer = new PrintWriter(socket.getOutputStream());
+			list.add(writer);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void run() {
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			while (true) {
+				String str = reader.readLine();
+				if (str == null)
+					break;
+				for (PrintWriter writer : list) {
+					writer.println(str);
+					writer.flush();
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			list.remove(writer);
+			try {
+				socket.close();
+			} catch (Exception ignored) {
+			}
+		}
+	}
 }
